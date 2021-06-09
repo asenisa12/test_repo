13d9474c389c3a2a92fb51de0a8c0a0cf57406c7
asenisa12 <asen0stoilov@gmail.com>
2021-06-09 10:52:32 +0300

    reader implementation

--- test_repo/main.c
+++ test_repo/main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "reader.h"
 
 
 int main()
 {
+    int rows, cols;
+   char **data = read_data(
+       "test.txt", &rows, &cols);
+
+
+    for (size_t i = 0; i < rows; i++)
+    {
+        printf("%s\n", data[i]);
+        free(data[i]);
+    }
+
+    free(data);
     
+
     return 0;
 }
--- test_repo/reader.c
+++ test_repo/reader.c
@@ -1,9 +1,30 @@
 #include "reader.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 char** read_data(const char *filename, int *rows, int *cols)
 {
     char ** data;
+    FILE *f = fopen(filename, "r");
+
+    fscanf(f, "%d %d\n", rows, cols);
+    data = malloc(*rows * sizeof(char*));
+    int i;
+    for ( i = 0; i < *rows; i++)
+    {
+        data[i] = malloc(*cols * sizeof(char));
+        //fread(data[i], sizeof(char), *cols, f);
+        
+        //fgets(data[i], *cols + 1, f);
+        //fgetc(f);
+
+        fscanf(f, "%s\n", data[i]);
+
+
+    }
+    
+    fclose(f);
+
 
     return data;
 }
