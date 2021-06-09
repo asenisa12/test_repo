416dac118ed2b81e9b6a45e35ad4869004a5ca5d
asenisa12 <asen0stoilov@gmail.com>
2021-06-09 10:34:02 +0300

    adding initial reader version

---
+++ test_repo/reader.c
@@ -0,0 +1,9 @@
+#include "reader.h"
+#include <stdio.h>
+
+char** read_data(const char *filename, int *rows, int *cols)
+{
+    char ** data;
+
+    return data;
+}
---
+++ test_repo/reader.h
@@ -0,0 +1,8 @@
+#ifndef READER_H
+#define READER_H
+
+
+char** read_data(const char *filename, int *rows, int *cols);
+
+
+#endif
